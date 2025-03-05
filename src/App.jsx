5fb62f9ff95749ac02fcf7b602d30b41ad113a1f
mohitchoudhary05 <choudharymohityt@gmail.com>
2025-03-05 11:50:25 +0530

    create form

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import ContactForm from './components/ContactForm/ContactForm'
+import Contact from './components/ContactHeader/Contact'
+import Navigation from './components/Navigation/Navigation'
+
+function App() {
+
+
+  return (
+    <div>
+
+     <Navigation/>
+     <Contact/>
+     <ContactForm/>
+    
+    </div>
+  )
+}
+
+export default App
